85c292aee5f4941a7ab5a2bb2311b8bbe7ee69e5
Dmitry Russ <dr@travelping.com>
2012-10-25 08:55:10 +0200

    add start/stop numbered instance

--- src/amazon_lib.erl
+++ src/amazon_lib.erl
@@ -1,39 +1,31 @@
 -module(amazon_lib).
--export([start/2, stop/1]).
--export([start/0, stop/0, info/0]).
+-export([start/0, stop/0, start/1, stop/1, info/0]).
 
 -export([init_ec2conn/0]).
 
 -include_lib("erlcloud/include/erlcloud.hrl").
 -include_lib("erlcloud/include/erlcloud_ec2.hrl").
 
-
 init_ec2conn() ->
     ApiKey = get_env(api_key),
     SecretKey = get_env(secret_key),
     erlcloud_ec2:new(ApiKey, SecretKey).
 
-start(_, _) ->
-    {ok, self()}.
-
-stop(_) ->
-    ok.
-
 start() ->
-    EC2 = init_ec2conn(),
-    InstanceIDs = get_env(instance_ids),
-    erlcloud_ec2:start_instances(InstanceIDs, EC2).
+    erlcloud_ec2:start_instances(get_env(instance_ids), init_ec2conn()).
+
+start(N) ->
+    erlcloud_ec2:start_instances([lists:nth(N, get_env(instance_ids))], init_ec2conn()).
 
 stop() ->
-    EC2 = init_ec2conn(),
-    InstanceIDs = get_env(instance_ids),
-    erlcloud_ec2:stop_instances(InstanceIDs, EC2).
+    erlcloud_ec2:stop_instances(get_env(instance_ids), init_ec2conn()).
+
+stop(N) ->
+    erlcloud_ec2:stop_instances([lists:nth(N, get_env(instance_ids))], init_ec2conn()).
 
 get_env(Id) ->
     {ok, Value} = application:get_env(?MODULE, Id),
     Value.
 
 info() ->
-    EC2 = init_ec2conn(),
-    InstanceIDs = get_env(instance_ids),
-    erlcloud_ec2:describe_instances(InstanceIDs, EC2).
+    erlcloud_ec2:describe_instances(get_env(instance_ids), init_ec2conn()).
